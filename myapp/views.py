02e595a47bf3b496f81561b9b57e4d0d449f37aa
LuisEmaAC <leacruzemanuel@hotmail.com>
2024-06-21 14:07:48 -0600

    create project, structure, add app, parameters and models, renders

---
+++ myapp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def hello(request, username):
+    return HttpResponse("<h1>Hello %s</h1>" % username)
+
+def Projects(request):
+    projects = list(Project.objects.values())
+    return JsonResponse(projects, safe=False)
+
+def tasks(request, title):
+    task = Task.objects.get(title=title)
+    return HttpResponse('task: %s' % task.title)
